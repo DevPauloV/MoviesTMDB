800b9846c2487e062bb608076603a85a7fca304d
Paulo Victor <vitorcanalle@gmail.com>
2025-05-01 14:46:14 -0300

    feat: adiciona rotas com BrowserRouter para Home, Movie e Search

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home/>} />
+          <Route path='movie/:id' element={<Movie/>} />
+          <Route path='search' element={<Search/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+  </React.StrictMode>,
 )
