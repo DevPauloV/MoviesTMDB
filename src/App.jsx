4b8d71cd6f1ead36537ec7d1dd67f05af979e7d4
Paulo Victor <vitorcanalle@gmail.com>
2025-05-01 18:44:34 -0300

    feat: adicionar navegação funcional entre páginas com react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
+import { Link, Outlet } from 'react-router-dom';
+import React from 'react';
+
 
 function App() {
- 
-
   return (
-    <div>
-      <h2> Movies</h2>
+    <div className="App">
+      <nav id='navbar'>
+        <h2>
+          <Link to="/">Movies</Link>
+        </h2>
+        <Link to="/movies/1">Movies</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      <h2>Movies</h2>
+      <Outlet />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App'
+import Home from './pages/Home'
+import Movie from './pages/Movie';
+import Search from './pages/Search';
+
 import './index.css'
 
 
@@ -11,9 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path='/' element={<Home/>} />
-          <Route path='movie/:id' element={<Movie/>} />
-          <Route path='search' element={<Search/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
         </Route>
       </Routes>
     </BrowserRouter>
