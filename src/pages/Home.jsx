63ed2bd158e4a9902698b2a50d108421d28b5855
Paulo Victor <vitorcanalle@gmail.com>
2025-05-02 23:09:21 -0300

    feat: buscar e exibir filmes mais bem avaliados na página Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,9 +11,26 @@ const Home = () => {
   const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
+
+    setTopMovies(data.results)
   }
 
-  return <div>Home</div>
+  useEffect(() => {
+    const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, [])
+
+  return (
+    <div className='container'>
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+      </div>
+    </div>
+  );
+
 
 };
 
