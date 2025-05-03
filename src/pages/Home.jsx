b9f6cd885d8c02d7c8ed553c27b521f3c8059a39
Paulo Victor <vitorcanalle@gmail.com>
2025-05-03 00:58:07 -0300

    feat: adiciona exibição de filmes top-rated usando MovieCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,34 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import MovieCard from '../components/MovieCard'
 
 const moviesUrl = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
-
 const Home = () => {
-  const [topMovies, setTopMovies] = useState([])
+  const [topMovies, setTopMovies] = useState([]);
 
   const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
     const data = await res.json();
-
-    setTopMovies(data.results)
-  }
+    setTopMovies(data.results);
+  };
 
   useEffect(() => {
-    const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
-
+    const topRatedUrl = `${moviesUrl}top_rated?api_key=${apiKey}&language=pt-BR`;
     getTopRatedMovies(topRatedUrl);
-  }, [])
+  }, []);
 
   return (
     <div className='container'>
       <h2 className="title">Melhores filmes:</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        {topMovies.length > 0 && topMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
     </div>
   );
-
-
 };
 
-export default Home
+export default Home;
