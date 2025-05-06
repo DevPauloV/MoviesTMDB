4f375e8bd116c8110dc60838775a1468dd5df636
Paulo Victor <vitorcanalle@gmail.com>
2025-05-05 23:56:33 -0300

    fix:  Adiciona busca de filmes e corrige interpolação da query na URL de busca de filmes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -16,7 +16,19 @@ const Search = () => {
 
   const [movies, setMovies] = useState([]);
   const query = searchParams.get("q");
+  
 
+   const getSearchedMovies = async (url) => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    };
+  
+    useEffect(() => {
+      const searchWithQueryURL = `${searchUrl}?api_key=${apiKey}&query=${query}&language=pt-BR`;
+      getSearchedMovies(searchWithQueryURL);
+    }, [query]);
+    
 
   return (
     <div className='container'>
@@ -24,10 +36,9 @@ const Search = () => {
         Resultados para: <span className="query-text">{query}</span>
       </h2>
       <div className="movies-container">
-        {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {movies.length === 0 && <p>Carregando...</p>}
+        {movies.length > 0 && 
+        movies.map((movie) => <MovieCard key={movie.id} movie={movie} />  )}
       </div>
     </div>
   );
