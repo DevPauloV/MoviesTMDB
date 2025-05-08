a3ece0a26a8661cf189bef7fe4ce4d9275fef6cd
Paulo Victor <vitorcanalle@gmail.com>
2025-05-07 22:37:06 -0300

    feat: exibe detalhes do filme na página Movie com formatação de valores

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useState, useEffect } from 'react';  
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {
   BsGraphUp,
   BsWallet2,
   BsHourglassSplit,
   BsFillFileEarmarkTextFill
-}  from 'react-icons/bs';
+} from 'react-icons/bs';
 
 import MovieCard from '../components/MovieCard';
 
@@ -26,14 +26,60 @@ const Movie = () => {
     setMovie(data);
   };
 
+  const formatCurrency = (number) => {
+    return number.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+  };
+
   useEffect(() => {
     const movieUrl = `${moviesUrl}${id}?${apiKey}`;
     getMovie(movieUrl);
   }, []);
 
 
-  return    <div>Movie</div>
-  
-}
+  return
 
-export default Movie
+  <div className='movie-page'>
+    {movie && (
+      <>
+        <MovieCard movie={movie} showLink={false} />
+        <p className='tagline'>{movie.tagline}</p>
+
+        //-----------------------------------------------------------
+        <div className="info">
+          <h3>
+            <BsWallet2 /> Orçamento:
+          </h3>
+          <p>{formatCurrency(movie.budge)}</p>
+        </div>
+        //-----------------------------------------------------------
+        <div className="info">
+          <h3>
+            <BsGraphUp /> Receita:
+          </h3>
+          <p>{formatCurrency(movie.revenue)}</p>
+        </div>
+        //-----------------------------------------------------------
+        <div className="info">
+          <h3>
+            <BsHourglassSplit /> Duração:
+          </h3>
+          <p>{movie.runtime} minutos</p>
+        </div>
+        //-----------------------------------------------------------
+        <div className="info description">
+          <h3>
+            <BsFillFileEarmarkTextFill /> Descrição:
+          </h3>
+          <p>{movie.overview}</p>
+        </div>
+
+
+      </>
+    )}
+  </div>;
+};
+
+export default Movie;
